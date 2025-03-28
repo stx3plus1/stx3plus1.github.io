24ab5de486065f8670fe80be64b8853cdb8984d3
stx3plus1 <stx4@ballpa.in>
2025-03-28 20:16:46 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    var contents = document.querySelectorAll("[id=content]");
+    var content = contents[contents.length - 1];
+    content.style.marginBottom = "0";
+});
