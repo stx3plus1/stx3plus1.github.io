86ee87acbfcbfab351fd48104b213f0d828c8e94
stx3plus1 <stx4@ballpa.in>
2025-05-22 03:47:45 +0100

    consistent website update

--- index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function () {
-    var contents = document.querySelectorAll("[id=content]");
-    var content = contents[contents.length - 1];
-    content.style.marginBottom = "0";
-});
